835f24200e6c2fde26363b86a440dca85b9251d8
Vivian Nadoti <vinadoti@gmail.com>
2022-05-19 10:48:13 -0300

    first commit

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,17 @@
+console.log("Trabalhando com variaveis");
+
+//JS é case sensitive ( linguagens sensíveis a maiúsculas e minúsculas )
+
+
+// podemos declarar variáveis de tipo número e texto com a mesma sintaxe:
+const idade = 23;
+const nome = "Vivian";
+let ano = 2022;
+
+
+// boa prática é inserir da declaração da variável, exemplo: const
+// Quando não inserimos a palavra const antes da variável, dizemos que ela pode ir para um escopo global.
+
+// Não fazer:
+//mes = "Janeiro";
+
---
+++ 3-operacoesAritmeticas.js
@@ -0,0 +1,1 @@
+console.log("Operações Aritméticas");
